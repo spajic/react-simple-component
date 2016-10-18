c8668c6e4ce74ee99fe7dad0f0e5721dfe768de8
Alexey Vasilyev <bestspajic@gmail.com>
2016-10-18 15:59:36 +0300

    Add CommentBox component

--- components.js
+++ components.js
@@ -10,7 +10,44 @@ class StoryBox extends React.Component { // Реакт-компонент все
     ); // Кавычки правда не нужны, из-за jsx
   }
 }
+
+
+
 ReactDOM.render(
   <StoryBox />, // Вызов компонента
   document.getElementById('story-app') // целевой элемент dom
+);
+
+class Comment extends React.Component {
+  render() {
+    return(
+      <div className="comment">
+        <p className="comment-header">{this.props.author}</p> {/* props будут переданы при вызове компонента*/}
+        <p className="comment-body">{this.props.body}</p>
+        <div className="comment-footer">
+          <a href="#" className="comment-footer-delete">Delete comment</a>
+        </div>
+      </div>
+    );
+  }
+}
+
+class CommentBox extends React.Component {
+  render() {
+    return(
+      <div className="comment-box">
+        <h3>Comments</h3>
+        <h4 className="comment-count">2 comments</h4>
+        <div className="comment-list">
+          <Comment author="Spajic" body="Hello!" /> {/* Переданные значения будут доступны компоненту в this.props */}
+          <Comment author="Maria" body="I Love You!" />
+        </div>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <CommentBox />,
+  document.getElementById('comments-box')
 );
